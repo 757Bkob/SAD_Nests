757Bkob/SAD_Nests

commit 460df250d27ce2010f4c5dc28f895e744235ea3b

more content in 3.0
nesting species improvements adding more props (spawnable at start, aggressive)

Effects applicable anywhere to make a species have an aggression event, change their aggression, and start/stop spawning nests

Many function shields

Confirmation of new invader behaviors

Code/NA_Helper.lua
@@ -51,25 +51,37 @@ function NA_Mod_Set(id)
 	end
 end
 
+function NA_create_runtime()
+	local species = Presets.NestingSpeciesPreset.Default --nests = ClassDescendantsList("TerritorialNest")
+	for _, v in ipairs(species) do
+		if v.nest_class and not Nest_Species_Savegame_Stats[v.id] then
+			local spawnflag = false
+			if v.nest_class == 'ScissorhandsNest' or v.nest_class == 'ShriekerNest' then
+				spawnflag = true
+			elseif v.nest_class == 'ConsortiumNest' and IsDlcAvailable('Robots') then
+				spawnflag = true
+			end
+			if not Nest_Species_Savegame_Stats[v.id] then
+				Nest_Species_Savegame_Stats[v.id] = {}
+			end
+			Nest_Species_Savegame_Stats[v.id][v.id .. '_nest_spawn_cd'] = 0
+			Nest_Species_Savegame_Stats[v.id][v.id .. '_evo_cd'] = 0
+			Nest_Species_Savegame_Stats[v.id][v.id .. '_stored_aggr'] = 0
+			Nest_Species_Savegame_Stats[v.id][v.id .. '_aggro_events'] = 0
+			Nest_Species_Savegame_Stats[v.id]['spawnflag'] = spawnflag
+			Nest_Species_Savegame_Stats[v.id]['aggressive'] = v.aggressive
+			Nest_Species_Savegame_Stats[v.id]['spawn_allowed'] = v.spawnable
+			Nest_Species_Savegame_Stats[v.id]['attacked_by_player'] = false
+			Nest_Species_Savegame_Stats[v.id]['attacked_by_others'] = {}
+		end
+	end
+end
+
 function Setup_nest_mod()
 	CreateGameTimeThread(function()
 		WaitMsg("DlcsLoaded")
-		local species = Presets.NestingSpeciesPreset.Default --nests = ClassDescendantsList("TerritorialNest")
-		for _, v in ipairs(species) do
-			if v.nest_class and not Nest_Species_Savegame_Stats[v.id] then
-				local spawnflag = false
-				if v.nest_class == 'ScissorhandsNest' or v.nest_class == 'ShriekerNest' then
-					spawnflag = true
-				elseif v.nest_class == 'ConsortiumNest' and IsDlcAvailable('Robots') then
-					spawnflag = true
-				end
-				Nest_Species_Savegame_Stats[v.id] = {}
-				Nest_Species_Savegame_Stats[v.id][v.id .. '_nest_spawn_cd'] = 0
-				Nest_Species_Savegame_Stats[v.id][v.id .. '_evo_cd'] = 0
-				Nest_Species_Savegame_Stats[v.id][v.id .. '_stored_aggr'] = 0
-				Nest_Species_Savegame_Stats[v.id][v.id .. '_aggro_events'] = 0
-				Nest_Species_Savegame_Stats[v.id]['spawnflag'] = spawnflag
-			end
+		if not Nest_Species_Savegame_Stats then
+			NA_create_runtime()
 		end
 		if Nest_scouting_quadrants == {} then
 			CreateMapGrid()

Code/NA_NestExpansion.lua
@@ -11,7 +11,7 @@ function Juno_Cancer(force)
 		end)
 	else
 		nests = MapGet(true, 'TerritorialNest', function(nest)
-			if GameTime() - cosnt.year > nest.spawned_on and nest.class ~= 'JunoNest' then
+			if GameTime() - const.Scale.years > nest.spawned_on and nest.class ~= 'JunoNest' then
 				return true
 			end
 		end)
@@ -19,8 +19,7 @@ function Juno_Cancer(force)
 	if #nests > 0 then
 		local roll = AsyncRand(#nests)
 		local to_convert = nests[roll]
-		Convert_nest_into(to_convert, 'nesting_juno')
-	elseif force then
+		return Convert_nest_into(to_convert, 'nesting_juno')
 	end
 end
 
@@ -484,12 +483,16 @@ function EnhancedTerritorialNest:change_nest_herd(force_evo)
 	return upgraded_flag
 end
 
+-- Moved this aggression to global var to allow runtime/save aggression changes
 function EnhancedTerritorialNest:can_be_aggressive(who)
-	if Presets.NestingSpeciesPreset.Default[self.nest_species].aggressive then
+	if not Nest_Species_Savegame_Stats then
+		NA_create_runtime()
+	end
+	if Nest_Species_Savegame_Stats[self.nest_species]['aggressive'] then
 		return true
 	elseif who == 'player' and self.attacked_by_player then
 		return true
-	elseif Presets.NestingSpeciesPreset.Default[who] and self.other_species_attacks[who] then
+	elseif Nest_Species_Savegame_Stats[self.nest_species]['attacked_by_others'].who then
 		return true
 	else
 		return false
@@ -497,21 +500,21 @@ function EnhancedTerritorialNest:can_be_aggressive(who)
 end
 
 function EnhancedTerritorialNest:RegisterTarget(unit, time)
+	if not Nest_Species_Savegame_Stats then
+		NA_create_runtime()
+	end
+	local wake_up_flag = false
 	if unit.player then
 		self.attacked_by_player = true
+		Nest_Species_Savegame_Stats[self.nest_species]['attacked_by_player'] = false
+		wake_up_flag = true
 	elseif unit.nest then
-		unit.nest.attacked_by_player = true
+		Nest_Species_Savegame_Stats[self.nest_species]['attacked_by_others'][unit.nest.nest_species] = true
+		wake_up_flag = true
 	end
-	local tags = unit['UnitTags']
-	if tags and self.state == 'asleep' and tags['Human'] then
-		DebugPrint("Nest registering a human attacker!\n")
+	if wake_up_flag and self.state ~= 'awake' and unit.nest.state ~= 'asleep' then
+		DebugPrint("Nest registering an attacker!\n")
 		self:SwitchState('sleepy')
-	end
-	if unit.nest then
-		DebugPrint("Nest registering another nest attacker!\n")
-		if self.state ~= 'awake' and unit.nest.state ~= 'asleep' then
-			self:SwitchState('sleepy')
-		end
 	end
 end
 
@@ -603,6 +606,9 @@ function EnhancedTerritorialNest:calculate_attack_strength(who)
 		local species = get_species_from_nest(self.class)
 		local banked = 0
 		local species_banked_aggr = species .. '_banked_aggr'
+		if not Nest_Species_Savegame_Stats then
+			NA_create_runtime()
+		end
 		if Nest_Species_Savegame_Stats[species] and Nest_Species_Savegame_Stats[self.nest_species][species_banked_aggr] then
 			banked = Nest_Species_Savegame_Stats[self.nest_species][species_banked_aggr]
 		else
@@ -763,6 +769,19 @@ function EnhancedTerritorialNest:IsQuadrantScouted(quad_no)
 	end
 end
 
+function EnhancedTerritorialNest:OnDie(reason)
+	if reason == "combat" or reason == "bleeding" then
+		local attacker = self.attack_received_by
+		if IsValid(attacker) and attacker.player then
+			MapForEach(self, range, "TerritorialNest", function(nest, me)
+				if nest.nest_species == me.nest_species then
+					self:nest_support_closest(nest)
+				end
+			end, self)
+		end
+	end
+end
+
 function EnhancedTerritorialNest:MarkScouted(other_species, time)
 	rawset(self, other_species, time)
 end
@@ -798,9 +817,13 @@ function ReportScoutingResults(quad_no, species, objects_to_report, player_found
 end
 
 -- TODO instead of cheat learning about the quadrant, send out a nesting unit to scout
-function EnhancedTerritorialNest:Scout_Specific_Quad(quad_no)
+function EnhancedTerritorialNest:Scout_Specific_Quad(quad_no,map_hacks)
 	DebugPrint("Nest is releasing a scouting unit!!\n")
 	local spawn_def = SpawnDefs['Nest_scout_passive']
+	if map_hacks then
+		print('Using the map hack version!')
+		spawn_def = SpawnDefs['Nest_scout_passive_map_hacks']
+	end
 	--used by invader to know what quadrant is to be scouted
 	-- And to track if the scout ever returned
 	if self.quadrant_scouting then
@@ -816,16 +839,6 @@ function EnhancedTerritorialNest:Scout_Specific_Quad(quad_no)
 	spawn_def = spawn_def:CreateInstance(instance)
 	local t = spawn_def:ResolveTarget()
 	spawn_def:ActivateSpawn(t, {}, 100)
-	--[[
-	if not Nest_scouting_quadrants[quad_no] then
-		CreateMapGrid()
-	end
-	local player_found = Player_presence_in_quadrant(quad_no)
-	local looking_for = {}
-	looking_for[#looking_for+1] = 'EnhancedTerritorialNest'
-	local all_nests = Cheat_find_all_in_quadrant(quad_no)
-	ReportScoutingResults(quad_no,self.nest_species,all_nests,player_found)
-	--]]
 end
 
 function MegaScout(nest)
@@ -834,8 +847,6 @@ function MegaScout(nest)
 	end
 end
 
--- Currently this will just cheat
--- I want to build out scouting behavior to allow the player to interact with a species learning about the map
 function EnhancedTerritorialNest:scout_nearest_quad()
 	DebugPrint("\nScouting closest usncouted quad!\n")
 	if not self.quadrant then
@@ -1016,28 +1027,6 @@ function EnhancedTerritorialNest:IsCloserToPlayer(other_nest)
 	return my_distance < other_distance
 end
 
-function EnhancedTerritorialNest:IsClosestToPlayer()
-	--local my_species = get_species_from_nest(self.class)
-	local compare_point = Get_center_of_survivors()
-	--print("Center of survivors is: ",compare_point,'\n')
-	local my_distance = self:GetDist2D(compare_point)
-	--print("I am this far away from it: ",my_distance,'\n')
-	local nest_dist = {}
-	MapForEach("map", self.class, function(nest, compare_point, me)
-		if nest ~= me then
-			nest_dist[#nest_dist + 1] = { nest = nest, dist = nest:GetDist2D(compare_point) }
-		end
-	end, compare_point, self)
-	local closest = self
-	--print(nest_dist,'\n')
-	for _, v in ipairs(nest_dist) do
-		if v.dist < my_distance then
-			closest = v.nest
-		end
-	end
-	return closest == self
-end
-
 -- A nest attacks the player only when it is the most-forward of its species; a nest behind a same-species ally sends
 -- support to that ally instead. "Behind" = the ally is closer to the survivor centre AND within an angular sector of
 -- this nest's own bearing from that centre. The angular gate is what stops support being sent to a nest ~180 degrees
@@ -1122,7 +1111,6 @@ end
 
 function EnhancedTerritorialNest:support_arrived(allied_unit)
 	--print("A unit supporting me has arrived!")
-	local my_strongest_tier = 0
 	if self.state == 'asleep' then
 		--print("This is going to try and wake me up!")
 		if not self.wake_up_alarms then
@@ -1388,7 +1376,7 @@ function UnitNesting:OnObjUpdate()
 end
 
 function UnitNesting:ReportScoutingResult()
-	quadrant = invader.target_quadrant
+	local quadrant = invader.target_quadrant
 	local day = GameTime()
 	local species = invader.location.nest_species
 	local list_of_found = self.observed_objects

Code/NA_Aggression.lua
@@ -35,6 +35,9 @@ function Aggression_log_faction(input)
 	local to_return = false
 	local threshold = Faction_aggression_threshold or Max(0, 6 - Get_difficulty_offset())
 	local count = 0
+	if not Nest_Species_Savegame_Stats then
+		NA_create_runtime()
+	end
 	if Nest_Species_Savegame_Stats[nesting_species] and Nest_Species_Savegame_Stats[nesting_species][nesting_species .. '_aggro_events'] then
 		count = Nest_Species_Savegame_Stats[nesting_species][nesting_species .. '_aggro_events']
 	else
@@ -103,7 +106,15 @@ local function can_spawn_nest(input)
 	if not nest_class_def then return nil end
 	local nest_classname = nest_class_def.class
 	DebugPrint("Checking if can spawn nest for species: " .. species_id)
-	DebugPrint("Checking how many nests of this clas exist on map: " .. nest_classname)
+	if not Nest_Species_Savegame_Stats then
+		NA_create_runtime()
+	end
+	local spawnable = Nest_Species_Savegame_Stats[species_id]['spawn_allowed']
+	if not spawnable then
+		DebugPrint("Nests are locked from spawning due to MapVar!!!")
+		return false
+	end
+	DebugPrint("Checking how many nests of this class exist on map: " .. nest_classname)
 	local count_total = MapCount("map", nest_classname)
 
 	-- In cases where the player needs to do something before spawning is enabled.
@@ -128,6 +139,9 @@ local function can_spawn_nest(input)
 end
 
 local function can_give_evo(input)
+	if not Nest_Species_Savegame_Stats then
+		NA_create_runtime()
+	end
 	local nest_species = find_nest_species(input)
 	if not nest_species then return nil end
 	local species_id = nest_species.id
@@ -155,6 +169,9 @@ local function can_give_evo(input)
 end
 
 function Aggression_up(input, location)
+	if not Nest_Species_Savegame_Stats then
+		NA_create_runtime()
+	end
 	local nest_species = find_nest_species(input)
 	if not nest_species then
 		nest_species = find_nest_species(Get_nest_species_by_region())

Code/NA_ExpeditionStuff.lua
@@ -9,29 +9,88 @@ function get_species()
 	return to_return
 end
 
-DefineClass.GiveExpeditionSpeciesEffectToSurvivor = {
+function get_nest_species()
+	local presets = Presets.NestingSpeciesPreset.Default
+	local to_return = {}
+	for _,v in ipairs(presets) do
+		if v.id then
+			table.insert_unique(to_return,v.id)
+		end
+	end
+	return to_return
+end
+
+DefineClass.NestingSpeciesAggressionEvent = {
 	__parents = { "Effect", },
 	__generated_by_class = "EffectDef",
 
 	properties = {
-		{ id = "species", name = "species", help = "What species is affected", editor = "choice", default = false, items = function (self) return get_species() end, },
-		{ id = "effect", name = "effect", help = "If species is getting more or less aggressive", editor = "choice", default = 'aggression up', items = {"aggression up","aggression down"}},
+		{ id = "species", name = "species", help = "What species is affected", editor = "choice", default = false, items = function (self) return get_nest_species() end, },
+		{ id = "aggression_up", name = "Is this aggression up?", help = "Set to false for it to be an aggression down event", editor = "bool", default = true,},
 	},
-	EditorView = Untranslated("<species> will have it's <effect>"),
-	Documentation = "Colony effects a species",
+	EditorView = Untranslated("<species> has an aggression event."),
+	Documentation = "Thing makes species mad or docile",
 	EditorNestedObjCategory = "Preset",
 }
 
-function GiveExpeditionSpeciesEffectToSurvivor:__exec(obj, context)
-	if self.effect == 'aggression up' then
+function NestingSpeciesAggressionEvent:__exec(obj, context)
+	if self.aggression_up then
 		Aggression_up(self.species)
-	elseif self.effect == 'aggression down' then
+	else
 		Aggression_down(self.species)
 	end
 end
 
-function GiveExpeditionSpeciesEffectToSurvivor:GetError()
-	if not self.effect then
+function NestingSpeciesAggressionEvent:GetError()
+	if not self.species then
+		return "Select an effect the species will do"
+	end
+end
+
+DefineClass.NestingSpeciesAggressionChange = {
+	__parents = { "Effect", },
+	__generated_by_class = "EffectDef",
+
+	properties = {
+		{ id = "species", name = "species", help = "What species", editor = "choice", default = false, items = function (self) return get_nest_species() end, },
+		{ id = "new_aggressive", name = "Now aggressive?", help = "If nests of this species can now attack the player", editor = "bool", default = false },
+	},
+	EditorView = Untranslated("<species> aggression change."),
+	Documentation = "Thing makes species aggressive or stops them from spawning",
+	EditorNestedObjCategory = "Preset",
+}
+
+function NestingSpeciesAggressionChange:__exec(obj, context)
+	if not Presets.NestingSpeciesPreset.Default[self.species] then return end
+	Presets.NestingSpeciesPreset.Default[self.species]['aggression'] = self.new_aggressive
+end
+
+function NestingSpeciesAggressionChange:GetError()
+	if not self.species then
+		return "Select an effect the species will do"
+	end
+end
+
+DefineClass.NestingSpeciesSpawnableChange = {
+	__parents = { "Effect", },
+	__generated_by_class = "EffectDef",
+
+	properties = {
+		{ id = "species", name = "species", help = "What species", editor = "choice", default = false, items = function (self) return get_nest_species() end, },
+		{ id = "new_spawnable", name = "Now spawnable?", help = "If nests of this species can now spawn on the map", editor = "bool", default = false},
+	},
+	EditorView = Untranslated("<species> spawning rules edit"),
+	Documentation = "Thing makes species spawnable or stops them from spawning",
+	EditorNestedObjCategory = "Preset",
+}
+
+function NestingSpeciesSpawnableChange:__exec(obj, context)
+	if not Presets.NestingSpeciesPreset.Default[self.species] then return end
+	Presets.NestingSpeciesPreset.Default[self.species]['spawn_allowed'] = self.new_spawnable
+end
+
+function NestingSpeciesSpawnableChange:GetError()
+	if not self.species then
 		return "Select an effect the species will do"
 	end
 end

Code/NA_NestPreset.lua
@@ -19,6 +19,7 @@ DefineClass.NestingSpeciesPreset = {
 		{ category = "Species",	id = "resource_list",	name = "Aggression resources", editor = "nested_list", default = false, base_class = "ResAmount", template = true, help = "Amount of a specific resource to trigger an aggression event.", },
 		{ category = "Species",	id = "spore_buildings",	name = "Spore Buildings", editor = "choice", default = false, items = function() return ClassDescendantsList('NestSpore') end, template = true, help = "The spore buildings this species stores resources in.", },
 		{ category = "Species",	id = "aggressive",	name = "Naturally Aggressive?", editor = "bool", default = true, template = true, help = "Will this species attack unprovoked?", },
+		{ category = "Species",	id = "spawnable",	name = "Nests spawning allowed from start?", editor = "bool", default = true, template = true, help = "Can nests of this species spawn from start of game?", },
 		{ category = "Prefab", id = "PrefabTags",   name = "Prefab Tags Any",        editor = "set",         default = empty_table, items = function() return PrefabTagsCombo() end, three_state = true },
 	},
 }

Code/NA_Unit_Invader_Expansion.lua
@@ -540,7 +540,16 @@ function InvaderBehaviourNestScout:OnAssign(invader, end_time)
 		end
 	end
 	if self.map_hack then
-		invader.observed_objects = Get_objs_in_quad(invader.target_quadrant, self.log_classes)
+		print("I got map hacks! Cheat finding everything!")
+		for _,type in ipairs(self.log_classes) do
+			local objects_found = Cheat_find_all_in_quadrant(invader.target_quadrant, type)
+			for _,obj in ipairs(objects_found) do
+				table.insert_unique(invader.observed_objects,obj)
+			end
+		end
+		print("Objects found:")
+		print(invader.observed_objects)
+		invader.player_found = Player_presence_in_quadrant(invader.target_quadrant)
 	end
 	invader:UpdateAttachedUI()
 end
@@ -600,7 +609,7 @@ function UnitDetection:detect_nearby()
 		self.detected_units = units
 	end
 	local range = self:GetDetectionRange()
-	self:GetMaxCollisionRadius(range + MaxLosTargetRadius) -- cache information about the surrounding, boosting the surf enum effectiveness
+	self:GetMaxCollisionRadius(range + const.MaxLosTargetRadius) -- cache information about the surrounding, boosting the surf enum effectiveness
 	self.los_checks = self
 		.los_max_checks                                 -- Limit the maximum allowed LOS checks. The enum is randomized, so we should eventually check all units.
 	local collection_idx = self:GetDetectCollectionIdx()

metadata.lua
@@ -25,7 +25,7 @@ return PlaceObj('ModDef', {
 	'id', "TGkJ3Tu",
 	'author', "Ark Builder",
 	'version_major', 3,
-	'version', 230,
+	'version', 234,
 	'lua_revision', 233360,
 	'saved_with_revision', 373414,
 	'entities', {
@@ -56,8 +56,8 @@ return PlaceObj('ModDef', {
 	},
 	'has_options', true,
 	'has_data', true,
-	'saved', 1780074621,
-	'code_hash', -7107918664334256810,
+	'saved', 1780254877,
+	'code_hash', 8074101603450800040,
 	'steam_id', "3479479720",
 	'TagAnimals', true,
 	'TagGameplay', true,

commit 20484aed34743bbb0586edefd7b67beb9bbe7169

Fix nest species-stats never populating (RegisterTarget nil-index crash)
Setup_nest_mod gated NA_create_runtime() behind `if not
Nest_Species_Savegame_Stats`, but that MapVar defaults to {} (truthy), so
the guard never fired and the per-species stats were never populated --
every Nest_Species_Savegame_Stats[id] access then read nil. Regression
from 460df25, which extracted the populate loop into NA_create_runtime
but changed the call from unconditional to that dead guard.

Call NA_create_runtime() unconditionally. It is idempotent (the per-id
guard at line 57 skips species already present), so it is safe on new
games and savegame loads, and forward-compatible for saves predating a
newly added species.

Also resolve nest_species lazily in RegisterTarget: it has no class
default and the engine UnitDetection sweep can reach RegisterTarget
before expand()/growth_event() assign it, which would index Stats[nil].
Mirrors the existing lazy-init at the other call sites.

Observed: ShriekerNest, NA_NestExpansion.lua:512, "attempt to index a
nil value", repeating from the detection update thread ~40s into a game.

Code/NA_Helper.lua
@@ -80,9 +80,12 @@ end
 function Setup_nest_mod()
 	CreateGameTimeThread(function()
 		WaitMsg("DlcsLoaded")
-		if not Nest_Species_Savegame_Stats then
-			NA_create_runtime()
-		end
+		-- Populate per-species stats unconditionally; NA_create_runtime skips any species already present
+		-- (line 57), so it is safe on both new games and savegame loads. The old
+		-- `if not Nest_Species_Savegame_Stats` guard was dead -- the MapVar defaults to {} (truthy), so it
+		-- never fired and every Nest_Species_Savegame_Stats[id] read nil (regression from commit 460df25,
+		-- which extracted this loop into NA_create_runtime but gated the call on an always-false condition).
+		NA_create_runtime()
 		if Nest_scouting_quadrants == {} then
 			CreateMapGrid()
 		end

Code/NA_NestExpansion.lua
@@ -500,9 +500,13 @@ function EnhancedTerritorialNest:can_be_aggressive(who)
 end
 
 function EnhancedTerritorialNest:RegisterTarget(unit, time)
-	if not Nest_Species_Savegame_Stats then
-		NA_create_runtime()
+	-- nest_species has no class default (set lazily in expand()/growth_event()); the engine UnitDetection
+	-- sweep can reach RegisterTarget first, leaving Nest_Species_Savegame_Stats[self.nest_species] a nil
+	-- index. Resolve it here as the other call sites do, and bail if the species can't be resolved.
+	if not self.nest_species then
+		self.nest_species = get_species_from_nest(self.class)
 	end
+	if not self.nest_species then return end
 	local wake_up_flag = false
 	if unit.player then
 		self.attacked_by_player = true